f897a44d70217a2e80255f822e2eeb8be95e8748
alexgrist14 <agurec.fasasf@gmail.com>
2019-10-03 13:54:34 +0300

    feat: add skills

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Contacts extends Component {
     render() {
         return (
-            <div>
+            <div className="contacts">
 
             </div>
         );
